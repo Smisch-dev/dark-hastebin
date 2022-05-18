601408ce596ea568528f528354d0eeb22b5c5fe1
Smisch-dev <82714256+Smisch-dev@users.noreply.github.com>
2022-05-18 22:24:02 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,46 @@
+{
+
+  "host": "0.0.0.0",
+  "port": 7777,
+
+  "keyLength": 10,
+
+  "maxLength": 400000,
+
+  "staticMaxAge": 86400,
+
+  "recompressStaticAssets": true,
+
+  "logging": [
+    {
+      "level": "verbose",
+      "type": "Console",
+      "colorize": true
+    }
+  ],
+
+  "keyGenerator": {
+    "type": "phonetic"
+  },
+
+  "rateLimits": {
+    "categories": {
+      "normal": {
+        "totalRequests": 500,
+        "every": 60000
+      }
+    }
+  },
+
+    "storage": {
+    "type": "file",
+    "host": "127.0.0.1",
+    "port": 6379,
+    "db": 2
+  },
+
+  "documents": {
+    "about": "./about.md"
+  }
+
+}
